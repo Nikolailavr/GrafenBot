1420cb010db5945b53db5c7911ceeb00026915b5
Nikolai <mail.@nlavrov.ru>
2024-08-29 16:34:27 +0700

    v1.05 Update messages

--- project/apps/sender/bot/handlers/commands/user.py
+++ project/apps/sender/bot/handlers/commands/user.py
@@ -2,10 +2,12 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message
 
 from apps.sender.bot.sender_data import SenderData
+from apps.sender.misc import const
 
 
 async def send_welcome(message: Message, bot: AsyncTeleBot):
-    await bot.reply_to(message, text='Приветствую! Я бот, который отправляет напоминание о перекусах первоклассников!')
+    await bot.send_message(chat_id=message.chat.id,
+                           text=const.TEXT_WELCOME)
 
 
 async def week_schedule(message: Message, bot: AsyncTeleBot):
@@ -14,18 +16,26 @@ async def week_schedule(message: Message, bot: AsyncTeleBot):
         mess = 'График на ближайшие 5 дней:'
         for date, value in send_data.items():
             mess += f'\n{date} - {value["text"]}'
-            if value["telegram_id"]:
-                mess += f'(@{value["telegram_id"]})'
+            # if value["telegram_id"]:
+            #     mess += f' (@{value["telegram_id"]})'
         await bot.send_message(chat_id=message.chat.id,
                                text=mess)
 
 
 async def children_schedule(message: Message, bot: AsyncTeleBot):
-    telegram_id = message.text.split(' ', 1)[1]
-    send_data = await SenderData.get_data_by_child(telegram_id)
-    if send_data:
-        mess = 'Ваш график на учебный год:'
-        for date, value in send_data.items():
-            mess += f'\n{value["date"]} - {value["text"]}'
-        await bot.send_message(chat_id=message.chat.id,
-                               text=mess)
+    mess = 'К сожалению, нет данных'
+    telegram_id = message.from_user.username
+    first_name = message.from_user.first_name
+    if telegram_id != '':
+        send_data = await SenderData.get_data_by_child(telegram_id)
+        if send_data:
+            mess = f'Уважаемый {first_name}!\n' if first_name else ''
+            mess += 'Ваш график на учебный год:'
+            for date, value in send_data.items():
+                mess += f'\n{value["date"]} - {value["text"]}'
+        else:
+            mess += f' для @{telegram_id}'
+    else:
+        mess = 'К сожалению, не смог найти ваше имя пользователя (username) в telegram'
+    await bot.send_message(chat_id=message.chat.id,
+                           text=mess)
