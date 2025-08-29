c5d4b7e9c838650d5fa1c4b14d151e29fa52d62e
Nikolai Lavrov <mail@nlavrov.ru>
2025-08-29 17:58:48 +0700

    v2.0 New version

--- src/apps/sender/bot/handlers/commands/admin.py
+++ src/apps/sender/bot/handlers/commands/admin.py
@@ -1,5 +1,4 @@
 import logging
-from unicodedata import digit
 
 from aiogram import Router, Dispatcher
 from aiogram.filters import Command, CommandObject
@@ -34,31 +33,15 @@ async def __tomorrow(message: Message, command: CommandObject):
     if message.from_user.id != settings.telegram.admin_chat_id:
         return
     class_num = command.args
-    if not isinstance(class_num, int):
+    if not class_num.isdigit():
         await message.answer("Need class_num")
     class_num = int(class_num)
     schedule = await ScheduleService.get_tomorrow(class_num)
-    class_data = await ClassService.get_class(class_num)
-    await SentMessage.msg_tomorrow(schedule, class_data.chat_id)
-
-
-# @router.message(Command("check"))
-# async def check_command(message: Message):
-#     date_str = message.text.split(' ', 1)[1]
-#     try:
-#         check_date = datetime.strptime(date_str, "%d-%m-%Y")
-#     except (ValueError, TypeError):
-#         await bot.reply_to(
-#             message,
-#             f"Некорректный формат даты {date_str}. Используйте DD-MM-YYYY."
-#         )
-#         return
-#     send_data = await SenderData.get_data_by_days(date_str)
-#     if send_data.get(date_str, None):
-#         text = send_data.get(date_str)
-#     else:
-#         text = 'На эту дату нет данных'
-#     await bot.send_message(chat_id=message.chat.id, text=text)
+    if schedule:
+        class_data = await ClassService.get_class(class_num)
+        await SentMessage.msg_tomorrow(schedule, class_data.chat_id)
+    else:
+        await message.answer("Завтра нет перекусов")
 
 
 def register_admin_handlers(dp: Dispatcher) -> None:
